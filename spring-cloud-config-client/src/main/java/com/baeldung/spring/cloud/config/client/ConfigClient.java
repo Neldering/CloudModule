c1c4a6606fe21e62e3847bd50917133b57bffbd4
Nicolas Eldering <eldering.nicolas@gmail.com>
2024-07-15 11:09:51 -0400

    XVP-demo

---
+++ spring-cloud-config-client/src/main/java/com/baeldung/spring/cloud/config/client/ConfigClient.java
@@ -0,0 +1,21 @@
+package com.baeldung.spring.cloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@SpringBootApplication
+public class ConfigClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+
+
+}
